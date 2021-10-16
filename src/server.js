ac7f541faaeb2bee0e62d26572fafab80d87469a
hyunkim <han.kim@kakao.com>
2021-10-16 16:09:21 +0900

    0.3

--- src/server.js
+++ src/server.js
@@ -2,9 +2,23 @@
 
 import express from "express";
 
+import path from 'path';
+const __dirname = path.resolve();
+
+
 const app = express();
 
+app.use(express.static('\public'));
 
-console.log("hello");
+app.set("view engine", "pug");
+app.set("views", __dirname + "/src/views");
+app.use("/public", express.static(__dirname + '/src/public'));
 
-app.listen(3001);
+console.log(__dirname)
+
+app.get("/", (req, res) => res.render("home"));
+
+
+const portNum = 3001;
+const handleListen = () => console.log(`Listening on http://localhost:${portNum}`)
+app.listen(portNum, handleListen);
